e8df90f62184c341315fb6757e86d444e26f8b61
ado durak <76266634+Alginest@users.noreply.github.com>
2021-09-06 14:06:34 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Hero from './components/Hero'
+import Navbar from './components/Navbar'
+import GlobalStyle from './globalStyles'
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <Navbar />
+      <Hero />
+    </>
+  )
+}
+
+export default App
